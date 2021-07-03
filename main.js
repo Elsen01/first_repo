00a9030121e66eb3a4ed9fadabdac20ff4a261c1
elsen <elsenaskerov8@gmail.com>
2021-07-03 11:47:14 +0400

    format

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ app.get('/api/course', (req, res) => {
 });
 
 app.get('/api/course/:id', (req, res) => {
-const course= courses.find(c=>c.id === parseInt(req.params.id));
-if (!course) res.status(404).send('There is no information about the id you specified.');
-res.send(course);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('There is no information about the id you specified.');
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
